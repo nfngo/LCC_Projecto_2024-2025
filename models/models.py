8f46603e163cb8983d82d1595cce6346efcec830
Nuno Oliveira <nfnoliveira@hotmail.com>
2025-04-22 15:16:30 +0100

    updated models to crop images and discard gradients

--- models/models.py
+++ models/models.py
@@ -33,7 +33,7 @@ def process_image(image: Image.Image,
     
     # # Load an image
     # image = Image.open(image_path)
-
+    faces_list = []
     # Detect faces in the image
     faces = detector.detect_faces(np.array(image))
 
@@ -41,23 +41,32 @@ def process_image(image: Image.Image,
     for i, face in enumerate(faces):
         x, y, width, height = face['box']
         
-        cropped_img = image.crop([x, y, x + width, y + height])
-        cropped_img.save(f'{destiny_path}/{image_name}.jpg')
+        size = 224, 224
+        # Discard crops that are too small (<75 x 75)
+        if width >= 75 and height >= 75:
+            cropped_img = image.crop([x, y, x + width, y + height])
+            # Create thumbnails of crops that are too big
+            if width >= 250 and height >= 250:
+                cropped_img.thumbnail(size)
+            cropped_img.save(f'{destiny_path}/{image_name}')
+            faces_list.append(cropped_img)
+    
+    return faces_list
 
 
-# Generate embeddings of all images to save to DB
-def gen_embeddings(files: list,
+# Given a list of images, generate vector embeddings and save them to DB
+def gen_embeddings(files: list[dict],
                    processor: ViTImageProcessor,
                    device: torch.device,
                    model: ViTModel):
-    embeddings = []
-    for img in files:
-        inputs = processor(images = img, return_tensors = "pt").to(device)
+    for img_data in files:
+        inputs = processor(images = img_data['img_obj'], return_tensors = "pt").to(device)
         with torch.no_grad():
             outputs = model(**inputs).last_hidden_state.mean(dim = 1).cpu().numpy()
-            embeddings.append(outputs)
+            # embeddings.append(outputs)
+            img_data['img_embedding'] = outputs.flatten().tolist()
 
-    return np.array(embeddings)
+    return files
 
 
 # Crop faces and generate embeddings to search 
@@ -67,6 +76,7 @@ def gen_embedding_img_to_search(image_path: str,
                          model: ViTModel,
                          detector: MTCNN):
     embeddings_to_search = []
+    searched_faces = []
     img = Image.open(image_path)
 
     # Detect faces in the image
@@ -76,9 +86,11 @@ def gen_embedding_img_to_search(image_path: str,
     for face in faces:
         x, y, width, height = face['box']
         cropped_img = img.crop([x, y, x + width, y + height])
+        searched_faces.append(cropped_img)
 
-        inputs = processor(images = cropped_img, return_tensors="pt").to(device)
-        img_to_search = model(**inputs).last_hidden_state.mean(dim=1)[0].tolist()
-        embeddings_to_search.append(img_to_search)
+        with torch.no_grad():
+            inputs = processor(images = cropped_img, return_tensors="pt").to(device)
+            img_to_search = model(**inputs).last_hidden_state.mean(dim=1).cpu().numpy().flatten().tolist()
+            embeddings_to_search.append(img_to_search)
 
-    return embeddings_to_search
+    return embeddings_to_search, searched_faces
