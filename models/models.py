b2626fb27ed4ffab982b1136ee2ebb0b82585686
Nuno Oliveira <nfnoliveira@hotmail.com>
2025-04-25 15:06:19 +0100

    Removed duplicated code from models

--- models/models.py
+++ models/models.py
@@ -5,14 +5,14 @@ import numpy as np
 from PIL import Image
 
 # Initialize models to process images and face detection
-def init_models(model_name: str = "base_patch16_224"):
+def init_models(model_name: str = "vit-base-patch16-224-in21k"):
     # Load ViT model
     device = torch.device("cpu")
     
-    if model_name == "dino_vits16":
-        # Model: facebook/dino-vits16
-        processor = ViTImageProcessor.from_pretrained('facebook/dino-vits16')
-        model = ViTModel.from_pretrained('facebook/dino-vits16').to(device)
+    if model_name == "dino-vitb16":
+        # Model: facebook/dino-vitb16
+        processor = ViTImageProcessor.from_pretrained('facebook/dino-vitb16')
+        model = ViTModel.from_pretrained('facebook/dino-vitb16')
     else:
         # Model: Vit Base Patch16 224 In21k
         processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
@@ -29,7 +29,9 @@ def init_models(model_name: str = "base_patch16_224"):
 def process_image(image: Image.Image,
                   detector: MTCNN,
                   destiny_path: str,
-                  image_name: str):
+                  image_name: str,
+                  save: bool = True,
+                  ignore_size: bool = False):
     
     # # Load an image
     # image = Image.open(image_path)
@@ -42,13 +44,14 @@ def process_image(image: Image.Image,
         x, y, width, height = face['box']
         
         size = 224, 224
-        # Discard crops that are too small (<75 x 75)
-        if width >= 75 and height >= 75:
+        # Discard crops that are too small (< 1/3 size)
+        if (width >= size[0]//3 and height >= size[1]//3) or ignore_size:
             cropped_img = image.crop([x, y, x + width, y + height])
             # Create thumbnails of crops that are too big
-            if width >= 250 and height >= 250:
+            if width > size[0] and height > size[1]:
                 cropped_img.thumbnail(size)
-            cropped_img.save(f'{destiny_path}/{image_name}')
+            if save:
+                cropped_img.save(f'{destiny_path}/{image_name}')
             faces_list.append(cropped_img)
     
     return faces_list
@@ -63,7 +66,6 @@ def gen_embeddings(files: list[dict],
         inputs = processor(images = img_data['img_obj'], return_tensors = "pt").to(device)
         with torch.no_grad():
             outputs = model(**inputs).last_hidden_state.mean(dim = 1).cpu().numpy()
-            # embeddings.append(outputs)
             img_data['img_embedding'] = outputs.flatten().tolist()
 
     return files
@@ -79,17 +81,17 @@ def gen_embedding_img_to_search(image_path: str,
     searched_faces = []
     img = Image.open(image_path)
 
-    # Detect faces in the image
-    faces = detector.detect_faces(np.array(img))
+    searched_faces = process_image(img,
+                                   detector, 
+                                   destiny_path = '', 
+                                   image_name = '', 
+                                   save = False, 
+                                   ignore_size = True)
 
-    # Save faces (cropped images)
-    for face in faces:
-        x, y, width, height = face['box']
-        cropped_img = img.crop([x, y, x + width, y + height])
-        searched_faces.append(cropped_img)
-
+    # Generate embeddings
+    for face in searched_faces:
         with torch.no_grad():
-            inputs = processor(images = cropped_img, return_tensors="pt").to(device)
+            inputs = processor(images = face, return_tensors="pt").to(device)
             img_to_search = model(**inputs).last_hidden_state.mean(dim=1).cpu().numpy().flatten().tolist()
             embeddings_to_search.append(img_to_search)
 
